8b6e78d7eb1c9b7eec0cef0ec332030b9da9551d
joetime <joetime@0badb4ec-d13f-11de-a69b-f5a595c35080>
2009-11-14 17:15:05 +0000

    Initial import. Basic functions to start game are working with simple Ajax interface.

---
+++ Risk/LoadBoard.aspx.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using RiskLib;
+using System.Drawing;
+
+namespace Risk
+{    
+    public partial class LoadBoard : System.Web.UI.Page
+    {
+        RiskGame Game 
+        {
+            /// Store game in session so it retains its object instance
+
+            get { return (RiskGame)Session["Game"]; }
+            set { Session["Game"] = value; }
+        }
+        
+        // Simple controls
+        Label players, state;
+        PlaceHolder placeHolder;
+
+        // Dynamically created controls
+        Dictionary<string, LinkButton> TerritoryLinks;
+        Dictionary<string, Label> PlayerNameLabels;
+        Dictionary<string, Label> TroopLabels;
+
+
+        protected void Page_Init(object sender, EventArgs e)
+        {
+            players = (Label)UpdatePanel1.FindControl("PlayersLabel");
+            state = (Label)UpdatePanel1.FindControl("StateLabel");
+            placeHolder = (PlaceHolder)UpdatePanel1.FindControl("PlaceHolder1");
+
+            /// Load controls in init so they are available during/after Page_Load
+            
+            CreateBoard();
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            /// Add events to controls created at runtime
+            
+            foreach (LinkButton lb in TerritoryLinks.Values)
+            {
+                lb.Click += TerritoryClick;
+            }
+        }
+
+        protected void TerritoryClick(object sender, EventArgs e)
+        {
+            LinkButton lb = (LinkButton)sender;
+            Game.Fortify(lb.Text);
+
+            UpdateLabels();
+        }
+
+
+        #region <UI methods>
+
+        private void CreateBoard()
+        {
+            TerritoryLinks = new Dictionary<string, LinkButton>();
+            PlayerNameLabels = new Dictionary<string, Label>();
+            TroopLabels = new Dictionary<string, Label>();
+
+            RiskBoard EmptyBoard = new RiskBoard(Server.MapPath("Risk.xml"));
+
+            Table T = new Table();
+            //if (Game == null) T.Visible = false;
+
+            foreach (BoardTerritory t in EmptyBoard.Territories
+                                            .OrderBy(n => n.Name)
+                                            .OrderBy(n => n.Continent.Name))
+            {
+                TableRow tr = new TableRow();
+                T.Rows.Add(tr);
+
+                TableCell ContinentCell = new TableCell();
+                TableCell TerritoryNameCell = new TableCell();
+                TableCell PlayerNameCell = new TableCell();
+                TableCell TroopsCell = new TableCell();
+                tr.Cells.Add(ContinentCell);
+                tr.Cells.Add(TerritoryNameCell);
+                tr.Cells.Add(PlayerNameCell);
+                tr.Cells.Add(TroopsCell);
+
+                Label clabel = new Label();
+                clabel.Text = t.Continent.Name;
+                ContinentCell.Controls.Add(clabel);
+
+                LinkButton lb = new LinkButton();
+                lb.ID = t.Name;
+                lb.Text = t.Name;
+                TerritoryNameCell.Controls.Add(lb);
+
+                Label nameLabel = new Label();
+                PlayerNameCell.Controls.Add(nameLabel);
+                Label troopsLabel = new Label();
+                TroopsCell.Controls.Add(troopsLabel);
+
+                TerritoryLinks.Add(t.Name, lb);
+                PlayerNameLabels.Add(t.Name, nameLabel);
+                TroopLabels.Add(t.Name, troopsLabel);
+            }
+
+            placeHolder.Controls.Add(T);
+        }
+
+        private void UpdateLabels()
+        {
+            StateLabel.Text = Game.GameState.ToString();
+            PlayersLabel.Text = Game.PlayersAsList();
+
+            if (Game.TurnInProgress)
+            {
+                foreach (LinkButton lb in TerritoryLinks.Values)
+                {
+                    if (Game.CurrentPlayer.Territories.Where(t => t.boardTerritory.Name == lb.Text).Count() == 0)
+                        lb.Enabled = false;
+                    else
+                        lb.Enabled = true;
+                }
+            }
+
+            // 'Game Board' Labels
+
+            foreach (BoardTerritory t in Game.Board.Territories)
+            {
+                try
+                {
+                    PlayerTerritory pt = Game.PlayerTerritories.Where(x => x.boardTerritory.Name == t.Name).Single();
+                    PlayerNameLabels[t.Name].Text = pt.Player.Name;
+                    TroopLabels[t.Name].Text = pt.Troops.ToString();
+                    ((TableRow)PlayerNameLabels[t.Name].Parent.Parent).BackColor = pt.Player.color;
+                }
+                catch
+                {
+                    PlayerNameLabels[t.Name].Text = "?";
+                    TroopLabels[t.Name].Text = "0";
+                    ((TableRow)PlayerNameLabels[t.Name].Parent.Parent).BackColor = Color.White;
+                }
+            }
+        }
+        
+        #endregion
+
+
+        #region  <game setup, simple calls to the RiskGame object>
+
+        protected void NewGame(object sender, EventArgs e)
+        {
+            Game = new RiskGame(Server.MapPath("Risk.xml"));
+
+            UpdateLabels();
+        }
+
+        protected void AddPlayer(object sender, EventArgs e)
+        {
+            TextBox t = (TextBox)UpdatePanel1.FindControl("TextBox1");
+            Game.AddPlayer(t.Text);
+
+            UpdateLabels();
+        }
+
+        protected void AssignTerritories(object sender, EventArgs e)
+        {
+            Game.AssignTerritoriesRandomly(new Random(DateTime.Now.Second));
+
+            UpdateLabels();
+        }
+
+        #endregion
+
+    }
+}
---
+++ RiskLib/RiskGame.cs
@@ -0,0 +1,237 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RiskLib;
+using System.Drawing;
+
+namespace RiskLib
+{
+    public class RiskGame 
+    {
+        public int[] InitalTroops = new int[] { 0, 0, 40, 35, 30, 25, 20 };
+        public Color[] PlayerColors = new Color[] { Color.LightGreen, Color.LightBlue, Color.LightPink };
+
+        public RiskBoard Board { get; private set; }
+        public List<RiskPlayer> Players { get; private set; }
+        public List<BoardTerritory> UnassignedTerritories { get; private set; }
+        public List<PlayerTerritory> PlayerTerritories { get; private set; }
+
+        public RiskGameState GameState { get; set; }
+
+        public RiskGame( string xmlPath )
+        {
+            Board = new RiskBoard(xmlPath);
+            Players = new List<RiskPlayer>();
+            PlayerTerritories = new List<PlayerTerritory>();
+
+            BoardTerritory[] tempArray = new BoardTerritory[Board.Territories.Count];
+            Board.Territories.CopyTo(tempArray);
+            UnassignedTerritories = tempArray.ToList();
+
+            GameState = new NotStarted(this);
+        }
+
+        public RiskPlayer CurrentPlayer 
+        {
+            get
+            {
+                if (CurrentPlayerIndex == -1) return null;
+                return Players[CurrentPlayerIndex];
+            }
+        }
+        private int CurrentPlayerIndex = -1;
+        public void SetCurrentPlayerIndex(int i)
+        {
+            CurrentPlayerIndex = i;
+        }
+
+        public void TurnOver()
+        {
+            CurrentPlayerIndex++;
+            if (CurrentPlayerIndex >= Players.Count)
+                CurrentPlayerIndex = 0;
+        }
+
+        public bool TurnInProgress
+        {
+            get { return (CurrentPlayer != null); }
+        }
+
+        public string PlayersAsList()
+        {
+            string str = "";
+
+            foreach (RiskPlayer p in Players)
+            {
+                str += p.Name + " "
+                    + p.Territories.Count.ToString() + " "
+                    + p.TotalTroops.ToString() + " "
+                    + p.NewTroops.ToString()
+                    + (p == CurrentPlayer ? "###" : "")
+                    + "<br>";
+            }
+            return str;
+        }
+    
+
+        // Calls to State Object
+
+        public void AddPlayer(string name)
+        {
+            GameState.AddPlayer(name);
+        }
+
+        public void AssignTerritoriesRandomly(Random r)
+        {
+            GameState.AssignTerritoriesRandomly(r);
+        }
+
+        public void GetNewTroops()
+        {
+            GameState.GetNewTroops();
+        }
+        public void Fortify(string territoryName)
+        {
+            GameState.Reinforce(territoryName);
+        }
+    }
+
+    #region Game States
+
+    public abstract class RiskGameState 
+    {
+        public RiskGame Game { get; set; }
+        
+
+        // Base class throws errors.
+
+        public virtual void AssignTerritoriesRandomly(Random rand)
+        {
+            throw new InvalidOperationException();
+        }
+        public virtual void AddPlayer(string name)
+        {
+            throw new InvalidOperationException();
+        }
+        public virtual void GetNewTroops()
+        {
+            throw new InvalidOperationException();
+        }
+        public virtual void Reinforce(string territoryName)
+        {
+            throw new InvalidOperationException();
+        }
+    }
+
+    public class NotStarted : RiskGameState 
+    {
+        public NotStarted(RiskGame game) { Game = game; }
+
+        public override void AddPlayer(string name)
+        {
+            Color c = Game.PlayerColors[Game.Players.Count];
+
+            Game.Players.Add(new RiskPlayer(name, Game.Board, c));
+
+            if (Game.Players.Count > 1)
+                Game.GameState = new HasEnoughPlayers(this);
+        }
+    }
+
+    public class HasEnoughPlayers : RiskGameState 
+    {
+        public HasEnoughPlayers(RiskGameState state) { Game = state.Game; }
+
+        public override void AssignTerritoriesRandomly(Random r) 
+        {
+            while (Game.UnassignedTerritories.Count > 0)
+            {
+                foreach (RiskPlayer p in Game.Players)
+                {
+                    BoardTerritory t = Game.UnassignedTerritories[r.Next(Game.UnassignedTerritories.Count)];
+
+                    Game.PlayerTerritories.Add(p.AssignTerritory(t));
+                    Game.UnassignedTerritories.Remove(t);
+                }
+            }
+
+            /// Select a player to go first, then change state
+            Game.SetCurrentPlayerIndex(r.Next(Game.Players.Count));
+
+            Game.GameState = new FirstReinforcementRound(this);
+        }
+
+        public override void AddPlayer(string name)
+        {
+            Color c = Game.PlayerColors[Game.Players.Count];
+
+            Game.Players.Add(new RiskPlayer(name, Game.Board, c));
+
+            if (Game.Players.Count > 1)
+                Game.GameState = new HasEnoughPlayers(this);
+        }
+    }
+
+    public class FirstReinforcementRound : RiskGameState 
+    {
+        public FirstReinforcementRound(RiskGameState state) { Game = state.Game; }
+
+        /* public override void GetNewTroops() 
+        {
+            if (G.CurrentPlayer.TotalTroops < G.InitalTroops[G.Players.Count])
+                G.CurrentPlayer.GiveTroops(1);
+        }*/
+
+        public override void Reinforce(string territoryName) 
+        {
+            /// During first fortify, turn is over after placing one troop
+
+            Game.CurrentPlayer.Reinforce(territoryName);
+            Game.TurnOver();
+
+            /// if all players have placed all of their troops, move into normal mode
+
+            foreach (RiskPlayer p in Game.Players)
+            {
+                if (p.TotalTroops < Game.InitalTroops[Game.Players.Count])
+                    return;
+            }
+
+            Game.GameState = new NormalTurnsGameState(this);
+        }
+    }
+
+    public class NormalTurnsGameState : RiskGameState 
+    {
+        public NormalTurn Turn { get; private set; }
+
+        public NormalTurnsGameState(RiskGameState state) 
+        { 
+            Game = state.Game;
+            Turn = new NormalTurn(Game);        /// Start the game!
+        }
+
+        public override void GetNewTroops() 
+        {
+            /// in this state, the player gets troops based on the following rules:
+            /// - 1 troop per three territories
+            /// - bonus troops for continents held
+            /// - 3 troop minimum per turn
+            /// (territory cards ignored for now)
+
+            int TerrTroops = Game.CurrentPlayer.Territories.Count / 3;
+            int ContinentTroops = Game.CurrentPlayer.ContinentTroops;
+            int TotalTroops = TerrTroops + ContinentTroops;
+
+            if (TotalTroops < 3) 
+                Game.CurrentPlayer.GiveTroops(TotalTroops);
+            else 
+                Game.CurrentPlayer.GiveTroops(3);
+        }
+    }
+
+    public class Completed : RiskGameState { }
+
+    #endregion
+}
