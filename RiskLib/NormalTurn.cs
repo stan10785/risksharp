e0a996a1869a9f10d87adc48eefade4996ae3a57
joetime <joetime@0badb4ec-d13f-11de-a69b-f5a595c35080>
2009-11-21 21:17:51 +0000

    many interface issues sorted out. Game and Turn State Pattern work. New game board image. Start of deck of risk cards.

--- RiskLib/NormalTurn.cs
+++ RiskLib/NormalTurn.cs
@@ -3,20 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace RiskLib
+namespace RiskLib 
 {
-    public class NormalTurn
+    public class NormalTurn 
     {
-        public TurnState State;
-        public RiskPlayer Player { get; private set; }
+        public NormalTurnState State;
+        
         public RiskGame Game { get; private set; }
 
         public NormalTurn(RiskGame g)
         {
             Game = g;
-            Player = Game.CurrentPlayer;
+            //Player = Game.CurrentPlayer;
             Player.NewTroopsReset();
             State = new ReinforceState(this);
+        }
+
+        public RiskPlayer Player { get { return Game.CurrentPlayer; } }
+
+        public List<string> GetSelectable()
+        {
+            return State.GetSelectable();
         }
 
         public void TerritorySelected(string territoryName)
@@ -25,10 +32,33 @@ namespace RiskLib
         }
     }
 
-    public abstract class TurnState 
+    public abstract class NormalTurnState 
     {
-        protected NormalTurn Turn { get; set; }
+        public NormalTurn Turn { get; protected set; }
 
+
+        
+
+        public virtual List<string> GetSelectable()
+        {
+            return new List<string>();
+        }
+
+        protected bool CanAttack()
+        {
+            /// Check if any attacks are possible
+
+            return (Turn.Game.CurrentPlayer.Territories.Where(
+                t => t.CanAttack).Count() > 0);
+        }
+
+        protected bool CanFortify()
+        {
+            /// Check if any fortifications are possile
+
+            return (Turn.Game.CurrentPlayer.Territories.Where(
+                t => t.CanFortify).Count() > 0);
+        }
 
         /// Base class throws error
 
@@ -40,18 +70,40 @@ namespace RiskLib
         {
             throw new InvalidOperationException();
         }
+        /// Controller properties
+        public virtual bool CanEndAttack() { return false; }
+
+        protected void TurnOver()
+        {
+            // assign card here
+
+            Turn.Game.TurnOver();
+            //Turn.Player = Turn.Game.CurrentPlayer;
+            Turn.State = new ReinforceState(this);
+        }
     }
 
     public class ReinforceState 
-        : TurnState 
+        : NormalTurnState 
     {
+
+        public ReinforceState(NormalTurnState t) : this(t.Turn) { }
         public ReinforceState(NormalTurn t) 
-        { 
+        {
             Turn = t;
             Turn.Player.GetNewTroops();
         }
+        
+        
 
-        public override void TerritorySelected(string territoryName)
+        public override List<string> GetSelectable() 
+        {
+            // Only a player's own territories are valid
+
+            return Turn.Game.CurrentPlayer.Territories.Select(t => t.boardTerritory.Name).ToList();
+        }
+
+        public override void TerritorySelected(string territoryName) 
         {
             PlayerTerritory pt = Turn.Game.PlayerTerritories.Where(n => n.boardTerritory.Name == territoryName).Single();
 
@@ -67,18 +119,49 @@ namespace RiskLib
 
             if (pt.Player.NewTroops == 0)
             {
-                Turn.State = new AttackState(Turn);
+                Turn.State = new AttackState(this);
             }
         }
     }
 
     public class AttackState 
-        : TurnState 
+        : NormalTurnState 
     {
+        /// <summary>
+        /// The logic here gets a little wishy washy. This may need its own state,
+        /// especially when we add the cards and retreat into the mix
+        /// </summary>
+
         public PlayerTerritory AttackingTerritory { get; private set; }
         public PlayerTerritory DefendingTerritory { get; private set; }
 
-        public AttackState(NormalTurn t) { Turn = t; }
+        public override bool CanEndAttack() { return true; }
+
+        public AttackState(NormalTurnState nts) 
+        { 
+            Turn = nts.Turn;
+        }
+
+        public override List<string> GetSelectable()
+        {
+            /// perhaps this should have its own state?
+
+            if (AttackingTerritory == null)
+            {
+                /// return all territories that can attack
+
+                return Turn.Game.CurrentPlayer.Territories
+                    .Where(t => t.CanAttack)
+                    .Select(t => t.boardTerritory.Name)
+                    .ToList();
+            }
+            else
+            {
+                // return the current attacking territory's potential targets
+
+                return AttackingTerritory.AttackableTerritories.Select(t => t.boardTerritory.Name).ToList();
+            }
+        }
 
         public override void TerritorySelected(string territoryName) 
         {
@@ -113,16 +196,26 @@ namespace RiskLib
 
         private void ExecuteAttack() 
         {
-            /// for now, attacking territory always wins, takes 1 troop
+            /// for now, attacking territory always wins, moves 1 troop
+            
             bool AttackSuccess = true;
             int TroopsToMove = 1;
 
             if (AttackSuccess)
             {
+                RiskPlayer ConqueredPlayer = DefendingTerritory.Player;
+
                 /// Defending territory now controlled by attacking player
 
                 DefendingTerritory.Conquered(AttackingTerritory.Player, TroopsToMove);
                 AttackingTerritory.ReduceForce(TroopsToMove);
+
+                /// Check for Player Defeated
+
+                if (ConqueredPlayer.Territories.Count == 0)
+                {
+                    Turn.Game.PlayerDefeated(ConqueredPlayer);
+                }
             }
             else
             {
@@ -130,26 +223,64 @@ namespace RiskLib
             }
 
 
-            /// Start a new Attack
+            /// Find correct state change
 
-            Turn.State = new AttackState(Turn);
+            if (!CanAttack() && !CanFortify())
+            {
+                base.TurnOver();
+            }
+            else if (!CanAttack())
+            {
+                Turn.State = new FortifyState(this);
+            }
+            else
+            {
+                Turn.State = new AttackState(this);
+            }
         }
 
         public override void EndAttack()
         {
             /// Go to Fortify mode
-
-            Turn.State = new FortifyState(Turn);
+            if (!CanFortify())
+            {
+                base.TurnOver();
+            }
+            else
+            {
+                Turn.State = new FortifyState(this);
+            }
         }
     }
 
     public class FortifyState 
-        : TurnState
+        : NormalTurnState
     {
         public PlayerTerritory SourceTerritory { get; private set; }
         public PlayerTerritory TargetTerritory { get; private set; }
 
-        public FortifyState(NormalTurn t) { Turn = t; }
+        public FortifyState(NormalTurnState nts) { Turn = nts.Turn; }
+
+        public override List<string> GetSelectable()
+        {
+            /// perhaps this should have its own state?
+
+            if (SourceTerritory == null)
+            {
+                /// return all territories that can fortify
+
+                return Turn.Game.CurrentPlayer.Territories
+                    .Where(t => t.CanFortify)
+                    .Select(t => t.boardTerritory.Name)
+                    .ToList();
+            }
+            else
+            {
+                // return the current source territory's potential targets
+
+                return SourceTerritory.FortifiableTerritories.Select(t => t.boardTerritory.Name).ToList();
+            }
+        }
 
         public override void TerritorySelected(string territoryName)
         {
@@ -183,4 +314,14 @@ namespace RiskLib
             Turn.Game.State.TurnOver();
         }
     }
+
+    //public class TurnOver
+    //    : NormalTurnState
+    //{
+    //    public TurnOver(NormalTurnState t)
+    //    {
+    //        Turn = t.Turn;
+            
+    //    }
+    //}
 }
